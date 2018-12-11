e3cc2987190fa48a0dc7b5e54b838dd9f90f84b9
ali.aktolun <ali.aktolun@sahabt.com>
2018-12-11 20:58:44 +0300

    pom selenium version update, chrome driver added

--- src/test/java/com/sahabt/Hooks.java
+++ src/test/java/com/sahabt/Hooks.java
@@ -6,6 +6,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -17,13 +19,24 @@ public class Hooks {
 
   @Before
   public void beforeTest() {
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setCapability("key", System.getProperty("key"));
-
-    try {
-      webDriver = new RemoteWebDriver(new URL(URL), capabilities);
-    } catch (MalformedURLException e) {
-      e.printStackTrace();
+    DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+    String key = System.getProperty("key", "");
+    if (key.isEmpty()) {
+      System.setProperty("webdriver.chrome.driver", "web_driver/chromedriver");
+      webDriver = new ChromeDriver();
+    } else {
+      capabilities.setCapability(CapabilityType.PLATFORM, "LINUX");
+      capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+      capabilities.setCapability(CapabilityType.VERSION, "LATEST");
+      capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
+      capabilities.setCapability("recordsVideo", true);
+      capabilities.setCapability("screenResolution", "FHD");
+      capabilities.setCapability("key", System.getProperty("key"));
+      try {
+        webDriver = new RemoteWebDriver(new URL(URL), capabilities);
+      } catch (MalformedURLException e) {
+        e.printStackTrace();
+      }
     }
     webDriver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
     webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
